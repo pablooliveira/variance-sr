004511fc3079b6594f750f1f609a67453f8f87a7
Pablo Oliveira <pablo@sifflez.org>
2022-09-01 15:15:04 +0200

    Use mca_int backend to simulate sr

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ for n in 250 500 1000 2000 4000 8000 16000 32000 64000 128000 256000 512000 1024
 
     ./variance_ieee $n 1 | sed -e "s/^/$n binary32 /" >> results.tab
 
-    VFC_BACKENDS="libinterflop_mca.so --mode=rr" ./variance_sr $n 29 | sed -e "s/^/$n sr /" >> results.tab
+    VFC_BACKENDS="libinterflop_mca_int.so --mode=rr" ./variance_sr $n 29 | sed -e "s/^/$n sr /" >> results.tab
 done
